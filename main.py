c4f8582763cc9316c34e154e95342b01e59f2a2b
user <kulakovzena294@gmail.com>
2023-12-08 15:28:48 +0200

    second-commit

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('ok')
+def count_symbols(line_with_spaces):
+    return len(line_with_spaces.replace(' ', '').replace('\n', ''))
+
+
+with open('text.txt', 'r') as file:
+    line_number = 1
+    for line in file:
+        print(f'line {line_number}: {len(line.split())} words, {count_symbols(line)} symbols')
+        line_number += 1
